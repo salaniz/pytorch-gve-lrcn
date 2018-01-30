664fec66d93b184a63924642c03b16eff7e5a71d
salaniz <stephan.alaniz@gmail.com>
2018-01-30 18:18:39 +0100

    Add input_size and output_size to PretrainedModel; Remove trainable new layer

--- models/pretrained_models.py
+++ models/pretrained_models.py
@@ -1,8 +1,10 @@
+import torch
 import torch.nn as nn
+from torch.autograd import Variable
 import torchvision.models as models
 
 class PretrainedModel(nn.Module):
-    def __init__(self, model_name, output_dim):
+    def __init__(self, model_name):
         """Load the pretrained ResNet-152 and replace top fc layer."""
         super(PretrainedModel, self).__init__()
 
@@ -12,10 +14,18 @@ class PretrainedModel(nn.Module):
                                       'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
                                       'vgg16', 'vgg16_bn', 'vgg19_bn', 'vgg19']
 
+        if model_name == 'inception_v3':
+            self.input_size = (3, 299, 299)
+        elif model_name.startswith('resnet') or model_name.startswith('vgg'):
+            self.input_size = (3, 224, 224)
+        else:
+            # Default to (i.e. 'try')
+            self.input_size = (3, 224, 224)
+
         if model_name not in self.supported_model_names:
             print("WARNING: The requested pretrained model was not tested " +
-                    "in this implementation. The code will still run if " +
-                    "torch supports this model.")
+                  "in this implementation. The code might still run if " +
+                  "torch supports this model.")
 
         model = getattr(models, model_name)(pretrained=True)
         layers = list(model.children())
@@ -23,26 +33,24 @@ class PretrainedModel(nn.Module):
         if isinstance(last_layer, nn.Sequential):
             last_layer = last_layer[0]
 
-        modules = layers[:-1] # delete the last fc layer.
+        # Delete the last layer.
+        modules = layers[:-1]
         self.pretrained_model = nn.Sequential(*modules)
         # Freeze all parameters of pretrained model
         for param in self.pretrained_model.parameters():
                 param.requires_grad = False
 
-        self.linear = nn.Linear(last_layer.in_features, output_dim)
-        #self.bn = nn.BatchNorm1d(output_dim, momentum=0.01)
-        self.init_weights()
+        self.output_size = self._get_output_size()
 
-    def init_weights(self):
-        """Initialize the weights."""
-        self.linear.weight.data.normal_(0.0, 0.02)
-        self.linear.bias.data.fill_(0)
+    def _get_output_size(self):
+        dummy_input = Variable(torch.rand(1, *self.input_size))
+        output = self(dummy_input)
+        output_size = output.data.view(-1).size(0)
+        return output_size
 
     def forward(self, images):
         """Extract the image feature vectors."""
-        features = self.retrained_model(images)
+        features = self.pretrained_model(images)
         features = Variable(features.data)
         features = features.view(features.size(0), -1)
-        #features = self.bn(self.linear(features))
-        features = self.linear(features)
         return features
