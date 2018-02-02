01e1aad8a9bb8e38d88c970b49f3ff48205aa62d
salaniz <stephan.alaniz@gmail.com>
2018-02-03 00:00:55 +0100

    Add LRCN model

---
+++ models/lrcn.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence
+
+from .pretrained_models import PretrainedModel
+
+class LRCN(nn.Module):
+    def __init__(self, pretrained_model_name, word_embed_size, hidden_size,
+                 vocab_size, is_factored=True):
+        """Set the hyper-parameters and build the layers."""
+        super(LRCN, self).__init__()
+        self.vision_model = PretrainedModel(pretrained_model_name)
+        self.word_embed = nn.Embedding(vocab_size, word_embed_size, padding_idx=0)
+        self.is_factored = is_factored
+
+        lstm1_input_size = word_embed_size
+        lstm2_input_size = hidden_size
+        if not self.is_factored:
+            # Vision features are input to 1st LSTM
+            lstm1_input_size += self.vision_model.output_size
+        else:
+            # Vision features are input to 2nd LSTM (when is_factored == True)
+            lstm2_input_size += self.vision_model.output_size
+
+        self.lstm1 = nn.LSTM(lstm1_input_size, hidden_size, batch_first=True)
+        self.lstm2 = nn.LSTM(lstm2_input_size, hidden_size, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+
+        self.input_size = (self.vision_model.input_size, vocab_size)
+        self.output_size = vocab_size
+
+    def init_weights(self):
+        """Initialize weights."""
+        self.word_embed.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
+
+    def forward(self, image_inputs, captions, lengths):
+        """Decode image feature vectors and generates captions."""
+        #print(image_inputs.shape)
+        image_features = self.vision_model(image_inputs)
+        image_features = image_features.unsqueeze(1)
+        embeddings = self.word_embed(captions)
+        #print(image_features.shape)
+        #print(embeddings.shape)
+        if not self.is_factored:
+            embeddings = torch.cat((image_features, embeddings), 2)
+            image_features = image_features.expand(-1, embeddings.size(1), -1)
+            packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
+            hiddens, _ = self.lstm1(packed)
+            hiddens, _ = self.lstm2(hiddens)
+        else:
+            packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
+            hiddens, _ = self.lstm1(packed)
+            unpacked_hiddens, new_lengths = pad_packed_sequence(hiddens, batch_first=True)
+            image_features = image_features.expand(-1, unpacked_hiddens.size(1), -1)
+            unpacked_hiddens = torch.cat((image_features, unpacked_hiddens), 2)
+            packed_hiddens = pack_padded_sequence(unpacked_hiddens, lengths,
+                    batch_first=True)
+            hiddens, _ = self.lstm2(packed_hiddens)
+
+        #packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
+        #hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
+        return outputs
+
+    def sample(self, features, states=None):
+        """Samples captions for given image features (Greedy search)."""
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(20):                                      # maximum sampling length
+            hiddens, states = self.lstm(inputs, states)          # (batch_size, 1, hidden_size),
+            outputs = self.linear(hiddens.squeeze(1))            # (batch_size, vocab_size)
+            predicted = outputs.max(1)[1]
+            sampled_ids.append(predicted)
+            inputs = self.word_embed(predicted)
+            inputs = inputs.unsqueeze(1)                         # (batch_size, 1, word_embed_size)
+        sampled_ids = torch.cat(sampled_ids, 1)                  # (batch_size, 20)
+        return sampled_ids.squeeze()
